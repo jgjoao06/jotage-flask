869c488f3b45ba6ffe72e416f174c3badafa6a59
João Gabriel Soares <joaogabrielsoares@poli.ufrj.br>
2022-06-18 11:26:56 -0300

    Arquivos para começar

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=servicos.py
+FLASK_ENV=development
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import request, render_template
+from __init__ import app
+import funcoes
+
+@app.route('/webhook/link_publico', methods=['GET'])
+def link_publico():
+    if request.method == 'GET':
+        print("Data received from Webhook is: ", request.json)
+        resp = request.json
+        email = resp['email'] 
+        link_publico = funcoes.retornaLink(email)
+        return link_publico
+
+@app.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+        
+
+app.run(host='0.0.0.0', port=8000)
