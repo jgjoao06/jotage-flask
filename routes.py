504cf6a4f665ca64bbd039826a6e36a48c55e8a4
João Gabriel Soares <joaogabrielsoares@poli.ufrj.br>
2022-07-09 21:28:56 -0300

    edit

--- routes.py
+++ routes.py
@@ -8,6 +8,11 @@ def index():
     if request.method == 'GET':
         return render_template('index.html')
 
+@app.route('/login', methods=['GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
 @app.route('/link_publico', methods=['GET'])
 def link_publico():
     if request.method == 'GET':
